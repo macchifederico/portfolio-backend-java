81b1e1a56b599caab9e8f39bfbe8b5de57f90ab9
macchifederico <federico.macchi@gmail.com>
2022-07-31 00:57:03 -0300

    cambio endpoint de frontend V2

--- src/main/java/fmportfolio/Controller/PersonaController.java
+++ src/main/java/fmportfolio/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://loginfirebase-22c59.web.app")
 @RestController
 @RequestMapping("/api/personas/")
 public class PersonaController {
--- src/main/java/fmportfolio/Controller/SkillsController.java
+++ src/main/java/fmportfolio/Controller/SkillsController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://loginfirebase-22c59.web.app")
 @RequestMapping("/api/skills/")
 public class SkillsController {
     
