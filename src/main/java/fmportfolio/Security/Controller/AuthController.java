4646a4c24e2f148b5adffd8e0eda5477966eedc9
macchifederico <federico.macchi@gmail.com>
2022-07-31 01:36:27 -0300

    cors

--- src/main/java/fmportfolio/Security/Controller/AuthController.java
+++ src/main/java/fmportfolio/Security/Controller/AuthController.java
@@ -29,8 +29,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://loginfirebase-22c59.web.app")
+
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     
     @Autowired
